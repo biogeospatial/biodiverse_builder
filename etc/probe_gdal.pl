4248952f1980b6b99021586812014a034d1cec98
Shawn Laffan <shawnlaffan@gmail.com>
2025-10-16 10:51:12 +1100

    More gdal probery feedback

--- etc/probe_gdal.pl
+++ etc/probe_gdal.pl
@@ -4,7 +4,12 @@ use warnings;
 use Alien::gdal;
 
 eval 'use Geo::GDAL::FFI';
-warn $@ if $@;
+if (my $e = $@) {
+    warn "Error encountered when loading Geo::GDAL::FFI\n$e";
+}
+else {
+    say "No error when loading Geo::GDAL::FFI";
+}
 
 
 my @libs = grep {/libgdal.+.dylib$/} Alien::gdal->dynamic_libs;
