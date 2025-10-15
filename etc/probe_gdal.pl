e7479df4cbeea406eae25127bdc1875ced671028
Shawn Laffan <shawnlaffan@gmail.com>
2025-10-15 17:14:52 +1100

    Add and use a gdal probe script

---
+++ etc/probe_gdal.pl
@@ -0,0 +1,15 @@
+use 5.036;
+use warnings;
+
+use Alien::gdal;
+
+eval 'use Geo::GDAL::FFI';
+warn $@ if $@;
+
+
+my @libs = grep {/$libgdal.+.dylib$/} Alien::gdal->dynamic_libs;
+my $libgdal = shift @libs;
+
+my @res = `otool -L $libgdal`
+say "otool -L $libgdal";
+say join "\n", @res;
