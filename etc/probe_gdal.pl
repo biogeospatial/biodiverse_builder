b71477241fc7dd645ae87619a85289266802af1e
Shawn Laffan <shawnlaffan@gmail.com>
2025-10-16 10:25:58 +1100

    fix typo

--- etc/probe_gdal.pl
+++ etc/probe_gdal.pl
@@ -10,6 +10,6 @@ warn $@ if $@;
 my @libs = grep {/libgdal.+.dylib$/} Alien::gdal->dynamic_libs;
 my $libgdal = shift @libs;
 
-my @res = `otool -L $libgdal`
+my @res = `otool -L $libgdal`;
 say "otool -L $libgdal";
 say join "\n", @res;
